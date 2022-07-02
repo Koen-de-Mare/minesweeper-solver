ca0da159de1ee45eee115087cd0d3d989dabbfc9
Koen de Mare <koenmare@gmail.com>
2022-07-02 22:44:15 +0200

    first -sort of- working state

--- solver.py
+++ solver.py
@@ -6,7 +6,7 @@ import mineField as mf
 
 # possible states of knowledge of a cell
 # using numerical values to simplify math
-CELL_MINE = 100
+CELL_FLAGGED = 100 # cells that are inferred to contain a mine
 # 0  no mines in the adjacent cells
 # 1
 # 2
@@ -18,8 +18,12 @@ CELL_MINE = 100
 # 8  all adjacent cells have mines
 CELL_UNKNOWN = 200
 
+# boolean values for mis-using numpy arrays
+TRUE = 1
+FALSE = 0
+
 def cell_knowledge_as_character(cell_knowledge: int) -> str:
-    if cell_knowledge == CELL_MINE:
+    if cell_knowledge == CELL_FLAGGED:
         return 'X'
     elif 0 <= cell_knowledge <= 8:
         return str(cell_knowledge)
@@ -29,42 +33,61 @@ def cell_knowledge_as_character(cell_knowledge: int) -> str:
         print('INVALID CELL KNOWLEDGE VALUE: {}'.format(cell_knowledge))
         assert(False)
 
-class SolverContext:
+class Solver:
     def __init__(self, width: int, height: int, number_of_mines: int):
         '''
         initialize the problem and the knowledge state of the solver
         '''
 
+        # properties of the board
         self.width = width
         self.height = height
         self.number_of_mines = number_of_mines
-        
+
+        # external minesweeper game
         self.mine_field = mf.MineField(width=width,height=height,number_of_mines=number_of_mines)
 
+        # tracks what information has been obtained while playing the game
         self.knowledge = np.full((width,height), CELL_UNKNOWN)
 
-    def is_in_bounds(self, column: int, row: int):
-        return (column >= 0) && (row >= 0) && (column < self.width) && (row < self.height)
-        
-    def sweep_cell(self, column: int, row: int):
-        assert(is_in_bounds(self, column, row))
-        
-        current_cell_knowledge = self.knowledge[column, row]
-        if current_cell_knowledge == CELL_MINE:
-            print('SWEEPING A CELL WHERE A MINE SHOULD BE')
-            self.mine_field.sweep_cell(column, row)
-            print('THIS SHOULD NEVER OCCUR')
-            assert(False)
-        elif 0 <= current_cell_knowledge <= 8:
-            assert(current_cell_knowledge == self.mine_field.sweep_cell(column, row))
-        elif current_cell_knowledge == CELL_UNKNOWN:
-            print('RISKY SWEEP')
-            self.knowledge[column, row] = self.mine_field.sweep_cell(column, row)
-            print('sweep succeeded')
-        else:
-            print('invalid knowledge state of a cell: {}'.format(current_cell_knowledge))
-            assert(False)
+        # tracks if information regarding cells has been changed since they were last checked
+        self.is_dirty = np.full((width,height), FALSE)
 
+        # queue of dirty cells to be checked later
+        self.dirty_queue = []
+
+        
+    def is_in_bounds(self, column: int, row: int):
+        return (column >= 0) and (row >= 0) and (column < self.width) and (row < self.height)
+    
+
+    def sweep_unknown_cell(self, column: int, row: int):
+        '''
+        Will try to sweep the cell.
+        Assumes that previous code has made sure that this cell has not been sweeped before and does not contain a mine.
+        '''
+        assert(self.is_in_bounds(column, row))
+        assert(self.knowledge[column, row] == CELL_UNKNOWN)
+
+        # PERFORM THE SWEEP
+        self.knowledge[column, row] = self.mine_field.sweep_cell(column, row)
+
+        # mark this and surrounding cells as dirty
+        self.mark_neighbourhood_dirty(column, row)
+
+        
+    def flag_unknown_cell(self, column: int, row: int):
+        '''
+        Flags the cell for containing mine
+        '''
+        assert(self.is_in_bounds(column, row))
+        assert(self.knowledge[column, row] == CELL_UNKNOWN)
+
+        self.knowledge[column, row] = CELL_FLAGGED
+
+        self.mark_neighbourhood_dirty(column, row)
+
+        
     def print_grid(self):
         print('-----')
         for row in range(self.height):
@@ -77,106 +100,170 @@ class SolverContext:
             print('') # newline after end of row
         print('-----')
 
-    def neighbours(column: int, row: int):
-        assert(is_in_bounds(self, column, row))
-
-        # all neighbours
-        surrounds_list = []
-        surrounds_list.push((column + 1, row))     #         east
-        surrounds_list.push((column + 1, row - 1)) # north - east
-        surrounds_list.push((column,     row - 1)) # north
-        surrounds_list.push((column - 1, row - 1)) # north - west
-        surrounds_list.push((column - 1, row))     #         west
-        surrounds_list.push((column - 1, row + 1)) # south - west
-        surrounds_list.push((column,     row + 1)) # south
-        surrounds_list.push((column + 1, row + 1)) # south - east
-
-        # filter neighbours for being in bounds
-        surrounds_list_2 = list(filter(lambda a: self.is_in_bounds(a.0, a.1), surrounds_list))
-
-        print(surrounds_list_2)
-                                
-        return surrounds_list_2
         
+    def neighbours(self, column: int, row: int) -> [int]:
+        '''
+        Gives a list of all neighbours of the cell at (column, row) that are in the bounds of the game board
+        '''
+        assert(self.is_in_bounds(column, row))
 
-def check_cell(solver_context: SolverContext, column: int, row: int):
-    '''
-    check if an implication on the surrounding cells can be made
-    '''
+        # all neighbours, even if they aren't in bounds
+        neighbour_list = []
+        neighbour_list.append((column + 1, row))     #         east
+        neighbour_list.append((column + 1, row - 1)) # north - east
+        neighbour_list.append((column,     row - 1)) # north
+        neighbour_list.append((column - 1, row - 1)) # north - west
+        neighbour_list.append((column - 1, row))     #         west
+        neighbour_list.append((column - 1, row + 1)) # south - west
+        neighbour_list.append((column,     row + 1)) # south
+        neighbour_list.append((column + 1, row + 1)) # south - east
 
-    assert(solver_context.is_in_bounds(column, row))
+        # filter to keep only the neighbours that are in bounds
+        neighbour_list_filtered = list(filter(lambda a: self.is_in_bounds(a[0], a[1]), neighbour_list))
+
+        return neighbour_list_filtered
+
     
-    current_cell_knowledge = solver_context.knowledge[column, row]
-
-    if current_cell_knowledge == CELL_MINE:
-        # no information to work with
-    elif current_cell_knowledge == CELL_UNKNOWN:
-        # no information to work with
-    elif 0 <= current_cell_knowledge <= 8:
-        N_neighbour_mines = current_cell_knowledge
-
-        # count number of surrouning cells that we know have a mine
-        # count number of surrounding cells that we don't know the knowledge of; the cells that might still contain a mine
-        neighbour_list = solver_context.neighbours(column, row)
+    def count_neighbours(self, column: int, row: int) -> (int, int):
+        '''
+        count number of surrouning cells that we know have a mine
+        count number of surrounding cells that we don't know the knowledge of; the cells that might still contain a mine
+        '''
+        assert(self.is_in_bounds(column, row))
+        
+        neighbour_list = self.neighbours(column, row)
 
         N_unknown_neighbours = 0
         N_found_neighbour_mines = 0
 
-        for neighbour in neithbour_list:
+        for neighbour in neighbour_list:
             (current_neighbour_column, current_neighbour_row) = neighbour
-            assert(solver_context.is_in_bounds(current_neighbour_column, current_neighbour_row)
-            current_neighbour_knowledge = solver_context.knowledge[current_neighbour_column, current_neightbour_row]
-            if current_neighbour_knowledge == CELL_MINE:
+            assert(self.is_in_bounds(current_neighbour_column, current_neighbour_row))
+
+            current_neighbour_knowledge = self.knowledge[current_neighbour_column, current_neighbour_row]
+            
+            if current_neighbour_knowledge == CELL_FLAGGED:
                 N_found_neighbour_mines += 1
             elif current_neighbour_knowledge == CELL_UNKNOWN:
                 N_unknown_neighbours += 1
-            elif:
+            else:
                 assert(0 <= current_neighbour_knowledge <= 8)
 
-        N_hidden_mines = N_neighbour_mines - N_found_neighbour_mines
+        return (N_unknown_neighbours, N_found_neighbour_mines)
 
-        if N_hidden_mines == 0:
-            # all unknown neighbours must be safe
+    
+    def sweep_unknown_neighbours(self, column: int, row: int):
+        '''
+        Sweeps all neighbours of the cell at (column, row) that are currently unknown.
+        '''
+        assert(self.is_in_bounds(column, row))
 
-            # TODO -------------------------------------------------------------------------------------------------------------------------------------------------
-            # sweep all unknown neighbours
-            # mark all unknown neighbours and neighbours of newly sweeped neighbours as dirty
-                   
-        elif N_hidden_mines < N_unknown_neighbours:
-            # nothing can be inferred from this information
-        elif N_hidden_mines == N_unknown_neighbours:
-            # all unknown neighbours contain mines
+        neighbour_list = self.neighbours(column, row)
+        for neighbour in neighbour_list:
+            (current_neighbour_column, current_neighbour_row) = neighbour
+            current_neighbour_knowledge = self.knowledge[current_neighbour_column, current_neighbour_row]
 
-            # TODO -------------------------------------------------------------------------------------------------------------------------------------------------
-            # flag all unknown neighbours as mines
-            # mark all neighbours of newly flagged mines as dirty
-            
-        else:
-            print('INVALID STATE')
-            assert(False)
-                   
-    else:
-        print('INVALID CELL KNOWLEDGE STATE: {}'.format(current_cell_knowledge))
+            if current_neighbour_knowledge == CELL_UNKNOWN:
+                self.sweep_unknown_cell(current_neighbour_column, current_neighbour_row)
+
+
+    def flag_unknown_neighbours(self, column: int, row: int):
+        '''
+        Sweeps all neighbours of the cell at (column, row) that are currently unknown.
+        '''
+        assert(self.is_in_bounds(column, row))
+
+        neighbour_list = self.neighbours(column, row)
+        for neighbour in neighbour_list:
+            (current_neighbour_column, current_neighbour_row) = neighbour
+            current_neighbour_knowledge = self.knowledge[current_neighbour_column, current_neighbour_row]
+
+            if current_neighbour_knowledge == CELL_UNKNOWN:
+                self.flag_unknown_cell(current_neighbour_column, current_neighbour_row)
 
         
+    def mark_dirty(self, column: int, row: int):
+        assert(self.is_in_bounds(column, row))
+
+        self.is_dirty[column, row] = TRUE
+        self.dirty_queue.append((column, row))
+
+        
+    def mark_neighbourhood_dirty(self, column: int, row: int):
+        assert(self.is_in_bounds(column, row))
+        
+        self.mark_dirty(column, row)
+
+        neighbour_list = self.neighbours(column, row)
+        for neighbour in neighbour_list:
+            (current_neighbour_column, current_neighbour_row) = neighbour
+            self.mark_dirty(current_neighbour_column, current_neighbour_row)
+
+        
+    def check_cell(self, column: int, row: int):
+        '''
+        Check if an implication on the content of surrounding cells can be made.
+        Does not assume anything about the previous logic of the cell.
+        '''
+        assert(self.is_in_bounds(column, row))
+
+        if self.is_dirty[column, row] == FALSE:
+            # the cell has been checked in the mean while: no need to check it again
+            return
+        
+        current_cell_knowledge = self.knowledge[column, row]
+
+        if current_cell_knowledge == CELL_FLAGGED:
+            pass # no information to work with
+        elif current_cell_knowledge == CELL_UNKNOWN:
+            pass # no information to work with
+        elif 0 <= current_cell_knowledge <= 8:
+            N_neighbour_mines = current_cell_knowledge
+            
+            (N_unknown_neighbours, N_found_neighbour_mines) = self.count_neighbours(column, row)
+            
+            N_hidden_mines = N_neighbour_mines - N_found_neighbour_mines
+            if N_hidden_mines == 0:
+                # all unknown neighbours must be safe
+                # note the special case where there are no unknown neighbours, making N_hidden_mines == 0 vacuously true
+                self.sweep_unknown_neighbours(column, row)
+            elif N_hidden_mines < N_unknown_neighbours:
+                pass # nothing can be inferred from this information
+            elif N_hidden_mines == N_unknown_neighbours:
+                # all unknown neighbours contain mines
+                self.flag_unknown_neighbours(column, row)
+            else:
+                print('INVALID STATE')
+                assert(False)
+        
+        else:
+            print('INVALID CELL KNOWLEDGE STATE: {}'.format(current_cell_knowledge))
+
+
+    def process_queue(self):
+        '''
+        Will check all cells that are in the queue (including new ones that will be added to the queue in the process)
+        '''
+        while len(self.dirty_queue) > 0:
+            (column, row) = self.dirty_queue.pop(0)
+            self.check_cell(column, row)
+            self.is_dirty[column, row] = FALSE
+
+        # check if the self.is_dirty array is indeed "cleared" to everything being clean
+        total = np.sum(self.is_dirty)
+        if total != 0:
+            print('problem, total is: {}'.format(total))
+            assert(False)
+
+            
 if __name__ == '__main__':
 
     print('Hello, world')
 
-    solver_context = SolverContext(10, 10, 10)
+    solver = Solver(10, 10, 10)
 
-    solver_context.sweep_cell(1,1)
-    print(solver_context.knowledge[1,1])
-    solver_context.print_grid()
-    
-    solver_context.sweep_cell(1,8)
-    print(solver_context.knowledge[1,8])
-    solver_context.print_grid()
-    
-    solver_context.sweep_cell(3,1)
-    print(solver_context.knowledge[3,1])
-    solver_context.print_grid()
-    
-    solver_context.sweep_cell(4,5)
-    print(solver_context.knowledge[4,5])
-    solver_context.print_grid()
+    solver.sweep_unknown_cell(1,1)
+    solver.print_grid()
+
+    solver.process_queue()
+    solver.print_grid()
